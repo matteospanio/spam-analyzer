871b5a4313a300c92a7a2c7235b88fa8dc6add36
Matteo <spartacus990@gmail.com>
2022-11-15 10:00:08 +0100

    add cli version input test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,3 +13,12 @@ def test_help(capsys, option):
         pass
     output = capsys.readouterr().out
     assert config['description'] in output
+
+@pytest.mark.parametrize("option", ("-V", "--version"))
+def test_version(capsys, option):
+    try:
+        spam_detector.main([option])
+    except SystemExit:
+        pass
+    output = capsys.readouterr().out
+    assert config['version'] in output
