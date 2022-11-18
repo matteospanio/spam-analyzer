66abf9ed3129fd76a0675f8c4a608bff8aa5d786
Matteo <spartacus990@gmail.com>
2022-11-18 10:48:04 +0100

    fix hard coded ip in test_cli

--- tests/test_analyzer_data_structures.py
+++ tests/test_analyzer_data_structures.py
@@ -1,4 +1,5 @@
 from spamdetector.analyzer.data_structures import Domain, MailAnalysis, MailAnalyzer
+import socket
 
 trustable_mail = 'tests/samples/97.47949e45691dd7a024dcfaacef4831461bf5d5f09c85a6e44ee478a5bcaf8539.email'
 spam = 'tests/samples/00.1d30d499c969369915f69e7cf1f5f5e3fdd567d41e8721bf8207fa52a78aff9a.email'
@@ -10,15 +11,16 @@ with open('conf/word_blacklist.txt', 'r') as f:
 class TestDomainMethods:
     
     domain = Domain('google.com')
+    ip_addr = socket.gethostbyname('google.com')
 
     def test_from_string(self):
         assert self.domain == Domain.from_string('google.com')
         
     def test_from_ip(self):
-        assert self.domain == Domain.from_ip('142.250.180.174')
+        assert self.domain == Domain.from_ip(self.ip_addr)
 
     def test_get_ip_address(self):
-        assert self.domain.get_ip_address() == '142.250.180.174'
+        assert self.domain.get_ip_address() == self.ip_addr
 
 
 class TestMailAnalyzer:
