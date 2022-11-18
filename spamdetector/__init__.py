ba057a634bd3246bedb49ffb2b3ef0a8c9ab7863
Matteo <spartacus990@gmail.com>
2022-11-18 10:49:38 +0100

    remove alternative config path

--- spamdetector/__init__.py
+++ spamdetector/__init__.py
@@ -11,12 +11,9 @@ from spamdetector.analyzer import *
 
 __version__ = "0.0.1"
 __config_path__ = path.join(path.expanduser('~'), '.config', 'spamdetector')
-__alternative_config_path__ = '/etc/spamdetector'
 
 __defaults__ = {
     "SPAMDETECTOR_CONF_PATH": __config_path__,
-    "SPAMDETECTOR_ALT_CONF_PATH": __alternative_config_path__,
     "SPAMDETECTOR_CONF_FILE": path.join(__config_path__, "config.yaml"),
     "SPAMDETECTOR_VERSION": __version__,
 }
-
