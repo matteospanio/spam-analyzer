9ae27d1237b7f37ece36664d0bca8ca7ed5014bf
Matteo <spartacus990@gmail.com>
2022-11-21 22:05:29 +0100

    add dns query for domain class, add date header rule

--- spamdetector/analyzer/data_structures.py
+++ spamdetector/analyzer/data_structures.py
@@ -1,5 +1,7 @@
 import mailparser, socket, yaml
+import dns.resolver, dns.name
 from dataclasses import dataclass
+from datetime import datetime
 
 import spamdetector.analyzer.utils as utils
 
@@ -10,21 +12,22 @@ class Domain:
     A Domain is a class representing an internet domain, here you can get information about the target domain
     """
 
-    name: str
+    name: dns.name.Name
 
-    def __init__(self, name) -> None:
+    def __init__(self, name: str) -> None:
         """The constructor resolves any domain alias to the real domain name:
         in fact common domain names are aliases for more complex server names that would be difficult to remember for common users,
         since there is not a direct method in the `socket` module to resolve domain aliases, we use the `gethostbyname` chained with the `gethostbyaddr` methods
         this way makes the instatiation of the class slower, but it is the only way to get the real domain name.
         """
         # TODO: add a cache for the domain names or find a better way to resolve domain aliases
-        try:
-            ip = socket.gethostbyname(name)
-            self.name = socket.gethostbyaddr(ip)[0]
-        except Exception:
-            # if the name is not a valid domain name, we just use it
-            self.name = name
+        # try:
+        #     ip = socket.gethostbyname(name)
+        #     self.name = socket.gethostbyaddr(ip)[0]
+        # except Exception:
+        #     # if the name is not a valid domain name, we just use it
+        #     self.name = name
+        self.name = dns.name.from_text(name)
 
     @staticmethod
     def from_string(domain_str: str):
@@ -58,15 +61,21 @@ class Domain:
 
     def get_ip_address(self) -> str:
         """Translate the domain name to its ip address"""
-        return socket.gethostbyname(self.name)
+        return dns.resolver.resolve(self.name, 'A')[0].to_text()
 
     def is_trustable(self):
         pass
 
+    def __eq__(self, __o: object) -> bool:
+        result, _, _, = self.name.fullcompare(__o.name)
+        if result in [1, 2, 3]:
+            return True
+        return False
 
 @dataclass
 class MailAnalysis:
     """A summary of the analysis of a mail"""
+    spamassassin: str
 
     file_path: str
     """The path of the file analyzed"""
@@ -93,11 +102,14 @@ class MailAnalysis:
     The Authentication-Warning header is used to indicate that the message has been modified in transit.
     """
     has_suspect_subject: bool
+    """It is `True` if the mail's subject contains a suspicious word or a gappy word (e.g. `H*E*L*L*O`)"""
+    subject_is_uppercase: bool
 
-    send_year: int
+    send_date: datetime | None
 
     # attachments
     has_attachments: bool
+
     is_attachment_executable: bool
 
     # data from body
@@ -105,7 +117,9 @@ class MailAnalysis:
     """It is `True` if the body contains a script tag or a callback function
     Email clients that support JavaScript can execute the script in the email.
     """
-    contains_http_links: bool
+    contains_links: bool
+    contains_mailto_links: bool
+    https_only: bool
     forbidden_words_percentage: float
     """The rate of forbidden words in the body of the mail, it is a float between 0 and 1
     The formula is: `forbidden_words_count / total_bodyy_words_count`
@@ -122,7 +136,7 @@ class MailAnalysis:
             return 'Spam'
         if (not self.has_spf and not self.domain_matches):
             return 'Spam'
-        if (self.has_spf or self.domain_matches) and self.contains_http_links and self.forbidden_words_percentage > 0.05:
+        if (self.has_spf or self.domain_matches) and not self.https_only and self.forbidden_words_percentage > 0.05:
             return 'Warning'
         else:
             return 'Ham'
@@ -147,12 +161,29 @@ class MailAnalysis:
             if not self.domain_matches:
                 score += weights['domain_matches']
 
-        score += 0 if self.contains_http_links and self.send_year < 2010 else weights['contains_http_links']
+        # verify send date
+        # if the date has valid format
+        if type(self.send_date) is datetime:
+            # if the date is in the future
+            if self.send_date.timestamp() > datetime.now().timestamp():
+                # then add penalty
+                score += weights['invalid_date']
+            if not utils.is_valid_tz(self.send_date):
+                score += weights['invalid_date']
+            
+            # if email is old, it often contains http links
+            score += 0 if (not self.https_only and self.send_date.year < 2010) or self.https_only else weights['contains_http_links']
+        else:
+            # date is not in RFC 2822 format
+            score += weights['invalid_date']
+        
+        score += -1 if self.https_only else 0
         score += weights['bad_subject'] if self.has_suspect_subject else 0
+        score += weights['uppercase_subject'] if self.subject_is_uppercase else 0
 
         # those fields are not so often used, they should have a minimal impact in score
-        score += 0 if self.has_dkim else weights['has_dkim']
-        score += 0 if self.has_dmarc else weights['has_dmarc']
+        score += -1 if self.has_dkim else weights['has_dkim']
+        score += -1 if self.has_dmarc else weights['has_dmarc']
 
         score += weights['auth_warn'] if self.auth_warn else 0
 
@@ -163,8 +194,8 @@ class MailAnalysis:
         score += weights['has_html'] if self.contains_html else 0
 
         # attachments scoring
-        score += 0.2 if self.has_attachments else 0
-        score += 1 if self.is_attachment_executable else 0
+        score += weights['has_attachments'] if self.has_attachments else 0
+        score += weights['attachment_executable'] if self.is_attachment_executable else 0
 
         return score
 
@@ -178,11 +209,14 @@ class MailAnalysis:
                 "domain_matches": self.domain_matches,
                 "auth_warn": self.auth_warn,
                 "has_suspect_subject": self.has_suspect_subject,
-                "send_year": self.send_year
+                "subject_is_uppercase": self.subject_is_uppercase,
+                "send_date": self.send_date
             },
             "body": {
                 "contains_script": self.contains_script,
-                "contains_http_links": self.contains_http_links,
+                "https_only": self.https_only,
+                "contains_mailto_links": self.contains_mailto_links,
+                "contains_links": self.contains_links,
                 "forbidden_words_percentage": self.forbidden_words_percentage,
                 "contains_html": self.contains_html,
                 "contains_form": self.contains_form,
@@ -192,19 +226,25 @@ class MailAnalysis:
                 "is_attachment_executable": self.is_attachment_executable
             },
             "is_spam": self.is_spam(),
-            "score": self.get_score()
+            "score": self.get_score(),
+            "spamassassin": self.spamassassin
         }
 
 
 class MailAnalyzer:
+    """Analyze a mail and return a `MailAnalysis` object, essentiaòòy it is a factory of `MailAnalysis`.
+    
+    The analysis is based on the presence of some headers and the content of the body.
+    """
+    
     def __init__(self, wordlist):
         self.wordlist = wordlist
 
     def analyze(self, email_path: str, add_headers: bool = False) -> MailAnalysis:
         email = mailparser.parse_from_file(email_path)
 
-        has_spf, has_dkim, has_dmarc, domain_matches, auth_warn, has_suspect_subject, send_year = utils.inspect_headers(email.headers, self.wordlist)
-        contains_http_links, contains_script, forbidden_words_percentage, has_form, contains_html = utils.inspect_body(email.body, self.wordlist, self.get_domain(email_path))
+        has_spf, has_dkim, has_dmarc, domain_matches, auth_warn, has_suspect_subject, subject_is_uppercase , send_date = utils.inspect_headers(email.headers, self.wordlist)
+        contains_links, contains_mailto, https_only, contains_script, forbidden_words_percentage, has_form, contains_html = utils.inspect_body(email.body, self.wordlist, self.get_domain(email_path))
         has_attachments, is_executable = utils.inspect_attachments(email.attachments)
 
         return MailAnalysis(
@@ -214,15 +254,19 @@ class MailAnalyzer:
             has_dmarc=has_dmarc,
             domain_matches=domain_matches,
             auth_warn=auth_warn,
-            contains_http_links=contains_http_links,
+            contains_links=contains_links,
+            contains_mailto_links=contains_mailto,
+            https_only=https_only,
             contains_script=contains_script,
             forbidden_words_percentage=forbidden_words_percentage,
             has_attachments=has_attachments,
             is_attachment_executable=is_executable,
             has_suspect_subject=has_suspect_subject,
+            subject_is_uppercase=subject_is_uppercase,
             contains_form=has_form,
-            send_year=send_year,
-            contains_html=contains_html
+            send_date=send_date,
+            contains_html=contains_html,
+            spamassassin=utils.inspect_spamassassin(email.headers)
         )
 
     def get_domain(self, email_path: str) -> Domain:
