7c6eeb0d3cb0b11fdf156a817606888519550acd
Matteo <spartacus990@gmail.com>
2022-11-23 19:46:57 +0100

    add cli options

--- spamdetector/cli/parser.py
+++ spamdetector/cli/parser.py
@@ -10,8 +10,14 @@ def add_args(parser: ArgumentParser, config: dict) -> None:
     parser.add_argument('-w', '--custom-weights', help='A file containing custom weights', default=config['weights'], type=FileType('r'))
     parser.add_argument('-v', '--verbose', help='More program output', action='store_true')
     parser.add_argument('-V', '--version', help='Show program version', action='version', version=f'%(prog)s {__version__}')
+    parser.add_argument('-d', '--debug', help='Show debug messages', action='store_true')
+    # add model configuration and training options
+    # parser.add_argument('-m', '--model', help='The model to use', default=config['files']['classifier'], type=FileType('rb'))
+
     # TODO: add csv output format
-    parser.add_argument('-o', '--output-format', help='Format output in a different way', choices=['json'], metavar='FORMAT', dest='output_format')
+    parser.add_argument('-fmt', '--output-format', help='Format output in a different way', choices=['json'], metavar='FORMAT', dest='output_format')
+    parser.add_argument('-o', '--output-file', help='Write output to a file', type=FileType('w'), metavar='FILE', dest='output_file')
+    parser.add_argument('--destination-dir', help='The directory where to save the output', metavar='DIRECTORY', default='.')
 
 def parse_args(args, config: dict) -> Namespace:
     parser = ArgumentParser(
