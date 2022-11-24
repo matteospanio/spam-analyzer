1695b4afc3f1e28b52d776ac69dc0cd9ea907d21
Matteo <spartacus990@gmail.com>
2022-11-24 12:11:58 +0100

    add `Date` class tests

--- tests/test_analyzer_data_structures.py
+++ tests/test_analyzer_data_structures.py
@@ -1,5 +1,6 @@
-from spamdetector.analyzer.data_structures import Domain, MailAnalysis, MailAnalyzer
-import socket
+from spamdetector.analyzer.data_structures import Domain, MailAnalysis, MailAnalyzer, Date
+import socket, pytest
+from dateutil.parser import parse
 
 trustable_mail = 'tests/samples/97.47949e45691dd7a024dcfaacef4831461bf5d5f09c85a6e44ee478a5bcaf8539.email'
 spam = 'tests/samples/00.1d30d499c969369915f69e7cf1f5f5e3fdd567d41e8721bf8207fa52a78aff9a.email'
@@ -43,7 +44,47 @@ class TestMailAnalysis:
         assert self.mail_ok_an.file_path == trustable_mail
 
     def test_mail_analysis_is_spam(self):
-        assert self.mail_ok_an.is_spam() == 'Ham'
+        assert self.mail_ok_an.is_spam() == False
 
     def test_mail_analysis_get_score(self):
         assert self.mail_ok_an.get_score() <= 0.0
+
+
+class TestDate:
+    
+    RFC_date = 'Wed, 17 Feb 2021 10:00:00 +0100'
+    invalid_date = 'Wed, 17 Feb 2021 10:00:00'
+    date_plus_0 = 'Wed, 17 Feb 2021 10:00:00 +0000'
+    invalid_utc = 'Wed, 17 Feb 2021 10:00:00 +1900'
+    empty_date = ''
+
+    def test_successful_date_creation(self):
+        date1 = Date(self.RFC_date)
+        date2 = Date(self.invalid_date)
+
+        assert date1.date == parse(self.RFC_date)
+        assert date2.date == parse(self.invalid_date)
+    
+    def test_RFC_2822_format(self):
+        date1 = Date(self.RFC_date)
+        date2 = Date(self.invalid_date)
+        date3 = Date(self.invalid_utc)
+        
+        assert date1.is_RFC2822_formatted() == True
+        assert date2.is_RFC2822_formatted() == False
+        assert date3.is_RFC2822_formatted() == True
+    
+    def test_is_tz_valid(self):
+        date1 = Date(self.RFC_date)
+        date2 = Date(self.invalid_date)
+        date3 = Date(self.invalid_utc)
+        date4 = Date(self.date_plus_0)
+        
+        assert date1.is_tz_valid() == True
+        assert date2.is_tz_valid() == False
+        assert date3.is_tz_valid() == False
+        assert date4.is_tz_valid() == True
+
+    def test_empty_date_creation(self):
+        with pytest.raises(ValueError):
+            Date(self.empty_date)
