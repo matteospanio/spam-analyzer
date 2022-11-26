bb5d550945dd04c3863e50617898c80477593491
Matteo <spartacus990@gmail.com>
2022-11-26 13:18:24 +0100

    update classifier class

--- spamdetector/analyzer/classifier.py
+++ spamdetector/analyzer/classifier.py
@@ -1,19 +1,47 @@
 import pickle
-from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
+
+HEADERS = [
+    'has_spf',
+    'has_dkim',
+    'has_dmarc',
+    'domain_matches',
+    'auth_warn',
+    'has_suspect_subject',
+    'subject_is_uppercase',
+    'send_date_is_RFC2822_compliant',
+    'send_date_tz_is_valid',
+    'has_received_date',
+    'uppercase_body',
+    'script',
+    'images',
+    'https_only',
+    'mailto',
+    'links',
+    'bad_words_percentage',
+    'html',
+    'form',
+    'polarity',
+    'subjectivity',
+    'attachments',
+    'attach_is_executable',
+    'is_spam'
+]
 
 class SpamClassifier:
-    def __init__(self, X_train, y_train):
-        self.clf = DecisionTreeClassifier()
-        self.clf.fit(X_train, y_train)
+
+    def __init__(self, path_to_model) -> None:
+        self.model = load_model(path_to_model)
 
     def predict(self, X_test):
-        return self.clf.predict(X_test)
+        X = pd.DataFrame(X_test, columns=HEADERS[:-1])
+        return self.model.predict(X)
 
 def save_model(model: SpamClassifier, path: str) -> None:
     with open(path, 'wb') as f:
         pickle.dump(model, f)
 
-def load_model(path: str) -> SpamClassifier:
+def load_model(path: str):
     with open(path, 'rb') as f:
         return pickle.load(f)
 
